158759f82d36d3fd3fa0482444fac31063543c97
jamezac <jamezacristancho@gmail.com>
2021-09-23 11:57:13 -0500

    Build Folder

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ function App() {
           <h1>Jaime Andrés Meza Cristancho</h1>
           <h5>DISEÑADOR INDUSTRIAL / MAGISTER EN EDUCACIÓN EN TECNOLOGÍA</h5>
         </div>
-        <img src="LOGO_min.png" className="App-star"/>        
+        <img src="LOGO_min.png" className="App-star" alt="logo"/>        
       </header>
       <main>
         <nav className="main"> 
@@ -25,8 +25,8 @@ function App() {
               <h2>Experiencia Laboral</h2>
               <p><strong>DOCENTE DE DIBUJO TÉCNICO </strong>Instituto Técnico Industrial Francisco José de Caldas, abril 2018 - Actualmente</p>
               <ul> 
-                <li><a href="https://sites.google.com/site/andresmezacristancho/archivo/2020/clases-2020" target="_blank" >Clases Virtuales 2020</a></li>
-                <li><a href="https://sites.google.com/site/andresmezacristancho/archivo/2019/proyectos" target="_blank" >Muestra Técnica 2019</a></li>
+                <li><a href="https://sites.google.com/site/andresmezacristancho/archivo/2020/clases-2020" target="_blank" rel="noopener noreferrer">Clases Virtuales 2020</a></li>
+                <li><a href="https://sites.google.com/site/andresmezacristancho/archivo/2019/proyectos" target="_blank" rel="noopener noreferrer">Muestra Técnica 2019</a></li>
               </ul>
               <p><strong>DOCENTE DE TECNOLOGÍA E INFORMÁTICA </strong>Colegio Carlos Pizarro Leongómez, febrero 2016 - Abril 2018</p>
             </div>
@@ -35,13 +35,13 @@ function App() {
               <p><strong>UNIVERSIDAD DISTRITAL FRACISCO JOSÉ DE CALDAS</strong></p>
               <p>Magister en Educación en Tecnología, 2018 - 2020</p>
               <ul>
-                  <li><a href="https://repository.udistrital.edu.co/handle/11349/22979" target="_blank" > Tesis de grado sobre la Estimulación de la Inteligencia Espacial</a></li>
-                  <li><a href="https://www.youtube.com/watch?v=mC59ibnB6Qc&t=16200s" target="_blank" > Ponente en el IV Encuentro de Investigadores y Docentes en Educación en Tecnología</a></li>
+                  <li><a href="https://repository.udistrital.edu.co/handle/11349/22979" target="_blank" rel="noopener noreferrer"> Tesis de grado sobre la Estimulación de la Inteligencia Espacial</a></li>
+                  <li><a href="https://www.youtube.com/watch?v=mC59ibnB6Qc&t=16200s" target="_blank" rel="noopener noreferrer"> Ponente en el IV Encuentro de Investigadores y Docentes en Educación en Tecnología</a></li>
                 </ul>
               <p><strong>UNIVERSIDAD NACIONAL DE COLOMBIA</strong></p>
               <p>Diseñador Industrial, 2006 - 2012</p>
               <ul>
-                  <li><a href="https://www.flickr.com/photos/jamezac/sets/72157631709711084/show/" target="_blank" >Proyectos académicos, practica empresarial en diseño de maquinaria industrial</a></li>                  
+                  <li><a href="https://www.flickr.com/photos/jamezac/sets/72157631709711084/show/" target="_blank" rel="noopener noreferrer">Proyectos académicos, practica empresarial en diseño de maquinaria industrial</a></li>                  
                 </ul>           
             </div>
             <div className="main-container-s_skills" title="Soft Skills">
@@ -53,13 +53,13 @@ function App() {
             <div className="main-container-languages" title="Idiomas">
               <h2>Idiomas</h2>
               <p>Español = Nativo</p>
-              <a href="https://platzi.com/p/Jamezac/ruta/13-idioma-ingles/diploma/detalle/" target="_blank" >Ingles = B2</a>
+              <a href="https://platzi.com/p/Jamezac/ruta/13-idioma-ingles/diploma/detalle/" target="_blank" rel="noopener noreferrer">Ingles = B2</a>
             </div>
             <div className="main-container-social" title="Redes Sociales">
-              <a href="https://www.youtube.com/user/jamezacristancho/videos" target="_blank" className="main-container-social-y"><img src="youtube_social_icon_red.png"/></a>
-              <a href="https://www.youtube.com/user/jamezacristancho/videos" target="_blank" >YouTube Chanel</a>
-              <a href= "https://www.linkedin.com/in/jamezac/" target="_blank" className="main-container-social-l"><img src="LI-In-Bug.png" className="main-container-social-l"/></a>
-              <a href="https://www.linkedin.com/in/jamezac/" target="_blank" >Linkedin Profile</a>              
+              <a href="https://www.youtube.com/user/jamezacristancho/videos" target="_blank" rel="noopener noreferrer" className="main-container-social-y"><img src="youtube_social_icon_red.png" alt="logo"/></a>
+              <a href="https://www.youtube.com/user/jamezacristancho/videos" target="_blank" rel="noopener noreferrer">YouTube Chanel</a>
+              <a href= "https://www.linkedin.com/in/jamezac/" target="_blank" rel="noopener noreferrer" className="main-container-social-l"><img src="LI-In-Bug.png" className="main-container-social-l" alt="logo"/></a>
+              <a href="https://www.linkedin.com/in/jamezac/" target="_blank" rel="noopener noreferrer">Linkedin Profile</a>              
             </div>
           </div>
         </nav>
